75bc53cba676a3bbab0e5bddfce4474b357aaf71
David Barcenas <davidbarcenasmx@gmail.com>
2022-11-24 11:41:56 -0600

    create api endpoints

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -1,8 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+interface Todo {
+  title: string;
+}
 
 @Component({
   selector: 'ngrx-nx-workshop-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
+
+  constructor(private http: HttpClient) {
+    this.fetch();
+  }
+
+  fetch() {
+    this.http.get<Todo[]>('/api/todos').subscribe((t) => (this.todos = t));
+  }
+
+  addTodo() {
+    this.todos.push({
+      title: `New todo ${Math.floor(Math.random() * 1000)}`,
+    });
+  }
+}

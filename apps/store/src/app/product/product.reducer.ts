57bce28d4db90ed7c488f29567768e2a9f11ce33
David Barcenas <davidbarcenasmx@gmail.com>
2022-12-05 21:50:42 -0600

    implement ngrx entities

--- apps/store/src/app/product/product.reducer.ts
+++ apps/store/src/app/product/product.reducer.ts
@@ -1,41 +1,30 @@
 import { createReducer, on } from "@ngrx/store";
 import * as apiActions from './actions'
 import { ProductModel } from "../model/product";
+import { createEntityAdapter, EntityAdapter, EntityState } from "@ngrx/entity";
 
 export const productFeatureKey = 'product'
 
-export interface GlobalState {
-  product: ProductState;
-}
 export interface ProductState {
-  products?: ProductModel[];
+  products: EntityState<ProductModel>;
 }
+
+// If your entity's id property is different you can specify it during
+// entity adapter creation.
+export const productAdapter: EntityAdapter<ProductModel> = createEntityAdapter()
+
 export const initialState: ProductState = {
-  products: undefined
+  products: productAdapter.getInitialState()
 };
 
 export const productsReducer = createReducer(
   initialState,
   on(apiActions.productsFetchedSuccess, (state, { products }) => ({
     ...state,
-    products: [...products]
+    products: productAdapter.upsertMany(products, state.products)
   })),
-  on(apiActions.productsFetchedError, (state) => ({
+  on(apiActions.singleProductFetchSuccess, (state, {product}) => ({
     ...state,
-    products: []
+    products: productAdapter.upsertOne(product, state.products)
   })),
-  on(apiActions.singleProductFetchSuccess, (state, {product}) => {
-    const productsClone = state.products ? [...state.products] : []
-    const indexOfProduct = productsClone.findIndex(p => p.id === product.id)
-    // Remove old one and replace with single product fetch,
-    if(indexOfProduct < 0) {
-      productsClone.push(product)
-    } else {
-      productsClone.splice(indexOfProduct, 1, product)
-    }
-    return {
-      ...state,
-      products: productsClone
-    }
-  })
 )
--- apps/store/src/app/product/product.selectors.ts
+++ apps/store/src/app/product/product.selectors.ts
@@ -1,21 +1,29 @@
 import { createFeatureSelector, createSelector } from "@ngrx/store";
-import { productFeatureKey, ProductState } from "./product.reducer";
+import { productAdapter, productFeatureKey, ProductState } from "./product.reducer";
 import { selectRouterParam } from "../router/router.selectors";
 
 const selectProductState = createFeatureSelector<ProductState>(productFeatureKey)
 
-export const selectProducts = createSelector(
+const selectProductsState = createSelector(
   selectProductState,
   state => state.products
+)
+
+const {selectAll, selectEntities} = productAdapter.getSelectors();
+
+export const selectProducts = createSelector(selectProductsState, selectAll);
+const selectProductsEntities = createSelector(
+  selectProductsState,
+  selectEntities
 )
 
 export const selectCurrentProductId = selectRouterParam('productId')
 
 export const selectCurrentProduct = createSelector(
-  selectProducts,
+  selectProductsEntities,
   selectCurrentProductId,
   (products, id) => {
     if(id == null || !products) return undefined;
-    return products.find(p => p.id === id)
+    return products[id]
   }
 )
